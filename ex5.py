b2216bf5ad25f1edd27495939bc762514d1a80b7
omrigold <omrigold@gmail.com>
2019-05-23 10:25:13 -0500

    4th & 5th exercises completed

---
+++ ex5.py
@@ -0,0 +1,17 @@
+name = 'Omri Goldshtrom'
+my_age = 35
+my_height = 70 # inches
+my_weight = 185 # lbs
+my_eyes = 'Hazel'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print(f"Let's talk about {name}.")
+print(f"He's {my_height * 2.5} cm tall")
+print(f"He's {my_weight / 2.2} kgs heavy")
+print("Actually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
