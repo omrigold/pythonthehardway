b7574185408736cf09ca0deb54f0225d1e17c04c
omrigold <omrigold@gmail.com>
2019-05-23 11:02:05 -0500

    ninth exercise completed

---
+++ ex9.py
@@ -0,0 +1,14 @@
+# Here's some strange new printing stuff
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
+
+print("The days: ", days)
+print("The months: ", months)
+
+print("""
+There's something going on here.
+With 3 double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines, 5, or 6.
+""")
